b948a0b5aa5d269f88132af9e10a8e87223e900d
KTGhosT <sa24610220@my.sliit.lk>
2025-11-05 00:51:05 +0530

    Initial Setup Progress

---
+++ src/main/java/com/Project/fasttrack/Controller/AuthController.java
@@ -0,0 +1,18 @@
+package com.project.fasttrack.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "auth/login"; // Points to templates/auth/login.html
+    }
+
+    @GetMapping("/signup")
+    public String signupPage() {
+        return "auth/signup"; // Points to templates/auth/signup.html
+    }
+}
